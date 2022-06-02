e0127c83dc3d28148c83ca8732c745a6251bdb09
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-06-02 23:27:02 +0100

    Added a for loop and different functions with forEach to insert letter clicked into tiles for all rows!

--- script.js
+++ script.js
@@ -1,62 +1,42 @@
 // Adding event listener to keyboard press
-let deferredPrompt;
-document.addEventListener("click", addLetter);
+let buttons = Array.from(document.querySelectorAll("button"));
+buttons.forEach((button) => {
+  button.addEventListener("click", addLetter);
+});
 
 let tileArray = document.querySelectorAll("p");
+let count = 0;
 
 function addLetter(event) {
   let letter = event.target.innerHTML;
 
   for (let i = 0; i < tileArray.length; i++) {
-    if (tileArray[0].innerHTML === "") {
-      tileArray[0].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[0].innerHTML !== "" && tileArray[1].innerHTML === "") {
-      tileArray[1].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[1].innerHTML !== "" && tileArray[2].innerHTML === "") {
-      tileArray[2].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[2].innerHTML !== "" && tileArray[3].innerHTML === "") {
-      tileArray[3].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[3].innerHTML !== "" && tileArray[4].innerHTML === "") {
-      tileArray[4].innerHTML = `${letter}`;
-      wordCorrectFirstRow(tileArray);
-      addLetter();
-    }
-    if (tileArray[4].innerHTML !== "" && tileArray[5].innerHTML === "") {
-      tileArray[5].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[5].innerHTML !== "" && tileArray[6].innerHTML === "") {
-      tileArray[6].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[6].innerHTML !== "" && tileArray[7].innerHTML === "") {
-      tileArray[7].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[7].innerHTML !== "" && tileArray[8].innerHTML === "") {
-      tileArray[8].innerHTML = `${letter}`;
-      addLetter();
-    }
-    if (tileArray[8].innerHTML !== "" && tileArray[9].innerHTML === "") {
-      tileArray[9].innerHTML = `${letter}`;
-      wordCorrectSecondRow(tileArray);
-      addLetter();
+    if (tileArray[i].innerHTML === "") {
+      tileArray[i].innerHTML = `${letter}`;
+      count++;
+      if (count === 5) {
+        wordCorrectFirstRow(tileArray);
+      } else if (count === 10) {
+        wordCorrectSecondRow(tileArray);
+      } else if (count === 15) {
+        wordCorrectThirdRow(tileArray);
+      } else if (count === 20) {
+        wordCorrectFourthRow(tileArray);
+      } else if (count === 25) {
+        wordCorrectFifthRow(tileArray);
+      } else if (count === 30) {
+        wordCorrectSixthRow(tileArray);
+      }
+      return;
     }
   }
+  wordCorrectFirstRow(tileArray);
 }
 
 function wordCorrectFirstRow(arr) {
   arr.forEach((tile, index) => {
     if (winningWord.includes(tile.innerHTML) && index < 5) {
-      tile.style.background = "#6AAA64";
+      tile.style.background = "#C9B458";
     } else if (!winningWord.includes(tile.innerHTML) && index < 5) {
       tile.style.background = "#787C7E";
     }
@@ -66,7 +46,7 @@ function wordCorrectFirstRow(arr) {
 function wordCorrectSecondRow(arr) {
   arr.forEach((tile, index) => {
     if (winningWord.includes(tile.innerHTML) && index > 4 && index < 10) {
-      tile.style.background = "#6AAA64";
+      tile.style.background = "#C9B458";
     } else if (
       !winningWord.includes(tile.innerHTML) &&
       index > 4 &&
@@ -77,8 +57,64 @@ function wordCorrectSecondRow(arr) {
   });
 }
 
+function wordCorrectThirdRow(arr) {
+  arr.forEach((tile, index) => {
+    if (winningWord.includes(tile.innerHTML) && index > 9 && index < 15) {
+      tile.style.background = "#C9B458";
+    } else if (
+      !winningWord.includes(tile.innerHTML) &&
+      index > 9 &&
+      index < 15
+    ) {
+      tile.style.background = "#787C7E";
+    }
+  });
+}
+
+function wordCorrectFourthRow(arr) {
+  arr.forEach((tile, index) => {
+    if (winningWord.includes(tile.innerHTML) && index > 14 && index < 20) {
+      tile.style.background = "#C9B458";
+    } else if (
+      !winningWord.includes(tile.innerHTML) &&
+      index > 14 &&
+      index < 20
+    ) {
+      tile.style.background = "#787C7E";
+    }
+  });
+}
+
+function wordCorrectFifthRow(arr) {
+  arr.forEach((tile, index) => {
+    if (winningWord.includes(tile.innerHTML) && index > 19 && index < 25) {
+      tile.style.background = "#C9B458";
+    } else if (
+      !winningWord.includes(tile.innerHTML) &&
+      index > 19 &&
+      index < 25
+    ) {
+      tile.style.background = "#787C7E";
+    }
+  });
+}
+
+function wordCorrectSixthRow(arr) {
+  arr.forEach((tile, index) => {
+    if (winningWord.includes(tile.innerHTML) && index > 24 && index < 30) {
+      tile.style.background = "#C9B458";
+    } else if (
+      !winningWord.includes(tile.innerHTML) &&
+      index > 24 &&
+      index < 30
+    ) {
+      tile.style.background = "#787C7E";
+    }
+  });
+}
+
 // Making an array from all buttons so I can access the innerHTML of each button and style later.
-let keyArray = Array.from(document.querySelectorAll("button"));
+/*let keyArray = Array.from(document.querySelectorAll("button"));
 let buttonValue = keyArray.map((button) => {
   return button.innerHTML;
 });
