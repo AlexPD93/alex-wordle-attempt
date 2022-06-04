7bae859df232f4fa0e4b70326eb9d3a82fcbcf4c
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-06-04 19:33:52 +0100

    Added press enter function

--- script.js
+++ script.js
@@ -38,84 +38,15 @@ function addLetter(event) {
   wordCorrectFirstRow(tileArray);
 }
 
-function wordCorrectFirstRow(arr) {
-  arr.forEach((tile, index) => {
+function wordCorrectFirstRow(letter) {
+  console.log(letter);
+  /*arr.forEach((tile, index) => {
     if (winningWord.includes(tile.innerHTML) && index < 5) {
       tile.style.background = "#C9B458";
     } else if (!winningWord.includes(tile.innerHTML) && index < 5) {
       tile.style.background = "#787C7E";
     }
-  });
-}
-
-function wordCorrectSecondRow(arr) {
-  arr.forEach((tile, index) => {
-    if (winningWord.includes(tile.innerHTML) && index > 4 && index < 10) {
-      tile.style.background = "#C9B458";
-    } else if (
-      !winningWord.includes(tile.innerHTML) &&
-      index > 4 &&
-      index < 10
-    ) {
-      tile.style.background = "#787C7E";
-    }
-  });
-}
-
-function wordCorrectThirdRow(arr) {
-  arr.forEach((tile, index) => {
-    if (winningWord.includes(tile.innerHTML) && index > 9 && index < 15) {
-      tile.style.background = "#C9B458";
-    } else if (
-      !winningWord.includes(tile.innerHTML) &&
-      index > 9 &&
-      index < 15
-    ) {
-      tile.style.background = "#787C7E";
-    }
-  });
-}
-
-function wordCorrectFourthRow(arr) {
-  arr.forEach((tile, index) => {
-    if (winningWord.includes(tile.innerHTML) && index > 14 && index < 20) {
-      tile.style.background = "#C9B458";
-    } else if (
-      !winningWord.includes(tile.innerHTML) &&
-      index > 14 &&
-      index < 20
-    ) {
-      tile.style.background = "#787C7E";
-    }
-  });
-}
-
-function wordCorrectFifthRow(arr) {
-  arr.forEach((tile, index) => {
-    if (winningWord.includes(tile.innerHTML) && index > 19 && index < 25) {
-      tile.style.background = "#C9B458";
-    } else if (
-      !winningWord.includes(tile.innerHTML) &&
-      index > 19 &&
-      index < 25
-    ) {
-      tile.style.background = "#787C7E";
-    }
-  });
-}
-
-function wordCorrectSixthRow(arr) {
-  arr.forEach((tile, index) => {
-    if (winningWord.includes(tile.innerHTML) && index > 24 && index < 30) {
-      tile.style.background = "#C9B458";
-    } else if (
-      !winningWord.includes(tile.innerHTML) &&
-      index > 24 &&
-      index < 30
-    ) {
-      tile.style.background = "#787C7E";
-    }
-  });
+  });*/
 }
 
 // Making an array from all buttons so I can access the innerHTML of each button and style later.
@@ -125,37 +56,31 @@ let buttonValue = keyArray.map((button) => {
   return button.innerHTML;
 });
 
-//Function that runs when a key is pressed
+//Function for when enter is pressed
+
+function pressEnter(letter) {
+  // Check count is divisible by 5 run word correct function
+  if (count % 5 === 0) {
+    wordCorrectFirstRow(letter);
+  } else console.log("error");
+}
+
+//Function that runs when a letter is pressed
 function logKey(e) {
   let letter = e.key;
   let keyCode = e.keyCode;
-  console.log(keyCode);
 
   for (let i = 0; i < tileArray.length; i++) {
-    if (
-      tileArray[i].innerHTML === "" &&
-      letter !== "Enter" &&
-      letter !== "Backspace" &&
-      keyCode >= 65 &&
-      keyCode <= 90
-    ) {
+    if (tileArray[i].innerHTML === "" && keyCode >= 65 && keyCode <= 90) {
       tileArray[i].innerHTML = `${letter}`;
       count++;
-      if (count === 6 && letter === "Enter") {
-        console.log(count);
-        wordCorrectFirstRow(tileArray);
-      } else if (count === 10 && letter === "Enter") {
-        wordCorrectSecondRow(tileArray);
-      } else if (count === 15) {
-        wordCorrectThirdRow(tileArray);
-      } else if (count === 20) {
-        wordCorrectFourthRow(tileArray);
-      } else if (count === 25) {
-        wordCorrectFifthRow(tileArray);
-      } else if (count === 30) {
-        wordCorrectSixthRow(tileArray);
-      }
       return;
+    }
+    if (keyCode === 13) {
+      pressEnter(tileArray[i].innerHTML);
+    }
+    if (keyCode === 8) {
+      backSpace();
     }
   }
 }
